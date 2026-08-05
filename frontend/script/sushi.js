9dc2bc6dcf6cfd6c89accbeb3d6383db9ecc35b6
Matheus Henrique Magalhães Rodrigues <dev.matheus.magalhaes@gmail.com>
2026-08-05 13:59:50 -0300

    pagina sushi, script da pagina e estilização

---
+++ frontend/script/sushi.js
@@ -0,0 +1,27 @@
+    const buttons = document.querySelectorAll('.btn-modal');
+    const closeBtns = document.querySelectorAll('.close-btn');
+    const overlays = document.querySelectorAll('.modal-overlay');
+
+    
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const targetId = btn.getAttribute('data-modal');
+            const targetModal = document.getElementById(targetId);
+            targetModal.classList.add('active');
+        });
+    });
+
+    
+    closeBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const modal = e.target.closest('.modal-overlay');
+            modal.classList.remove('active');
+        });
+    });
+
+   
+    window.addEventListener('click', (e) => {
+        if (e.target.classList.contains('modal-overlay')) {
+            e.target.classList.remove('active');
+        }
+    });
